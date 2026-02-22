11fa3f361b66c43fd08ce4cc2305b1766e90e016
NotSwedishBacon <mbacon89@gmail.com>
2026-02-22 19:01:45 +0100

    Create symlinks for default wallpapers
    Added symlinks for various default wallpaper locations.

--- files/scripts/wallpaper.sh
+++ files/scripts/wallpaper.sh
@@ -5,6 +5,14 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-# Your code goes here.
+# Symlink all default wallpaper locations
 ln -sf /usr/share/wallpapers/oxide/default.jxl /usr/share/backgrounds/default.jxl
 ln -sf /usr/share/wallpapers/oxide/default-dark.jxl /usr/share/backgrounds/default-dark.jxl
+ln -sf /usr/share/wallpapers/oxide/default.jxl /usr/share/backgrounds/images/default.jxl
+ln -sf /usr/share/wallpapers/oxide/default-dark.jxl /usr/share/backgrounds/images/default-dark.jxl
+ln -sf /usr/share/wallpapers/oxide/default.jxl /usr/share/backgrounds/images/default-5_4.jxl
+ln -sf /usr/share/wallpapers/oxide/default-dark.jxl /usr/share/backgrounds/images/default-dark-5_4.jxl
+ln -sf /usr/share/wallpapers/oxide/default.jxl /usr/share/backgrounds/images/default-16_9.jxl
+ln -sf /usr/share/wallpapers/oxide/default-dark.jxl /usr/share/backgrounds/images/default-dark-16_9.jxl
+ln -sf /usr/share/wallpapers/oxide/default.jxl /usr/share/backgrounds/images/default-16_10.jxl
+ln -sf /usr/share/wallpapers/oxide/default-dark.jxl /usr/share/backgrounds/images/default-dark-16_10.jxl
